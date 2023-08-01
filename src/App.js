5cbce14ddcaa58dc32b606a813af9ed5a660eedb
lhelmi <7m.helmi@gmail.com>
2023-08-01 15:42:19 +0700

    update auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
 import 'upkit/dist/style.min.css'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+// (1) import Provider
+import { Provider } from 'react-redux';
+
+import store from './app/store';
 import Home from './pages/Home';
 
 
 function App() {
   return (
-  <div>
+  <Provider store = {store}>
     <Router>
       <Routes>
         <Route path="/" Component={ Home }/>
       </Routes>
     </Router>
-  </div>
+  </Provider>
   );
  }
 
